42d54b1030b383f947d797c44765a4dffe12e554
Pedro <plm@dazito.com>
2016-02-28 00:15:20 +0000

    Delete and setIfNotExists

--- akkademy-db-client/src/main/java/com/dazito/java/akkademy/client/Client.java
+++ akkademy-db-client/src/main/java/com/dazito/java/akkademy/client/Client.java
@@ -3,7 +3,9 @@ package com.dazito.java.akkademy.client;
 import akka.actor.ActorSelection;
 import akka.actor.ActorSystem;
 import akka.pattern.Patterns;
+import com.dazito.java.dakkabase.messages.DeleteMessage;
 import com.dazito.java.dakkabase.messages.GetRequest;
+import com.dazito.java.dakkabase.messages.SetIfNotExists;
 import com.dazito.java.dakkabase.messages.SetRequest;
 import scala.compat.java8.FutureConverters;
 
@@ -26,5 +28,13 @@ public class Client {
 
     public CompletionStage<Object> get(String key) {
         return FutureConverters.toJava(Patterns.ask(remoteDb, new GetRequest(key), 5000));
+    }
+
+    public CompletionStage<Object> delete(String key) {
+        return FutureConverters.toJava(Patterns.ask(remoteDb, new DeleteMessage(key), 5000));
+    }
+
+    public CompletionStage<Object> setIfNotExists(String key, Object value) {
+        return FutureConverters.toJava(Patterns.ask(remoteDb, new SetIfNotExists(key, value), 5000));
     }
 }
--- akkademy-db-client/src/test/java/com/dazito/java/akkademy/client/ClientIntegrationTest.java
+++ akkademy-db-client/src/test/java/com/dazito/java/akkademy/client/ClientIntegrationTest.java
@@ -2,7 +2,9 @@ package com.dazito.java.akkademy.client;
 
 import org.junit.Test;
 
+import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Created by daz on 27/02/2016.
@@ -16,5 +18,28 @@ public class ClientIntegrationTest {
         Integer result = (Integer) ((CompletableFuture) client.get("123")).get();
 
         assert (result == 123);
+    }
+
+    @Test
+    public void itShouldDeleteRecord() throws Exception {
+        CompletableFuture completableFuture = (CompletableFuture) client.delete("123");
+        String result = (String) completableFuture.get();
+
+        assert (Objects.equals(result, "123"));
+    }
+
+    @Test(expected = ExecutionException.class)
+    public void getValueShouldFail() throws Exception {
+        CompletableFuture completableFuture = (CompletableFuture) client.get("-1");
+        Integer result = (Integer) completableFuture.get();
+    }
+
+    @Test
+    public void setIfNotExist() throws Exception {
+        client.setIfNotExists("does not exist", "a non existing value");
+        CompletableFuture completableFuture = (CompletableFuture) client.get("does not exist");
+        String result = (String) completableFuture.get();
+
+        assert (Objects.equals(result, "a non existing value"));
     }
 }
